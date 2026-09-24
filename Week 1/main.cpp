85e579f4987d0186e4f37e26d3b64a263869c9a2
Zel1483 <ramadhano.hayzel@gmail.com>
2026-09-24 10:34:16 +0700

    revisi
    menjadikan 1

--- Week 1/main.cpp
+++ Week 1/main.cpp
@@ -1,1 +1,22 @@
+#include "advance_round.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
+int main() {
+    srand(static_cast<unsigned int>(time(nullptr)));
+
+    int money = 0;
+    const int TOTAL_ROUNDS = 3;
+
+    std::cout << "=== RUN START ===" << std::endl;
+
+    for (int round = 1; round <= TOTAL_ROUNDS; round++) {
+        advanceRound(round, money);
+    }
+
+    std::cout << "\n=== RUN END ===" << std::endl;
+    std::cout << "Final money: " << money << std::endl;
+
+    return 0;
+}
